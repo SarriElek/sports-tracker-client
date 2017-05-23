40716e793cafae7d4a31450b9b194055d6d06209
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-23 21:33:36 +0000

    Added feature; API calls

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import chat from './chat';
 import user from './user';
+import sidebar from './side_bar';
 
 const sportsApp = combineReducers({
   user,
-  chat
+  chat,
+  sidebar
 });
 
 export default sportsApp;
