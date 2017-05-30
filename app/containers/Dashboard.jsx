b62e8a75d9340ea3b6e86d26da046c9c149007fc
Edurne Berastegi Zabalza <edurne.berastegi@gmail.com>
2017-05-30 00:39:49 +0000

    moved add user card call to sidebar

--- app/containers/Dashboard.jsx
+++ app/containers/Dashboard.jsx
@@ -1,50 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import CardBox from '../components/cards/CardBox';
 import { joinRoom, leaveRoom } from '../actions/chat';
 import { togglePlayByPlay, removeCard, repositionCard } from '../actions/cards';
 import { socketAction } from '../middlewares/websocket';
-import api from '../lib/api';
-import { receiveCard } from '../actions/card';
 
-class Dashboard extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    const HOST = location.origin.replace('8081', '8080');
-    const { dispatch } = this.props;
-    api.get('http/leagues/mlb/users/34').then((response) => {
-      response.response.forEach(card => {
-        api.post(`${HOST}/leagues/${card.league}/games/${card.gameId}`, card).then((response) => {
-          dispatch(receiveCard(response.response));
-        });
-      })
-    });
-  }
-
-  render() {
-    return (
-      <CardBox { ...this.props } />
-    )}
-};
+const Dashboard = props => (
+  <CardBox { ...props } />
+);
 
 const mapStateToProps = state => ({
   allCards: state.cards,
   socket: state.chat.socket,
   chatActive: state.chat.active !== 0
 });
-
-const mapDispatchToProps = dispatch => ({
-  leaveRoom: socketAction(joinRoom),
+const mapDispatchToProps = {
+  leaveRoom: socketAction(leaveRoom),
   togglePlayByPlay,
   removeCard,
   repositionCard,
-  dispatch,
   joinRoom: socketAction(joinRoom)
-});
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
